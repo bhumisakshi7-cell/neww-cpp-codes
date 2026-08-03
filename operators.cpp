2fb102b34988fd64af5fe5522d615b9235514f14
bhumisakshi7-cell <bhumisakshi7@gmail.com>
2026-08-03 22:52:38 +0530

    Add files via upload

---
+++ operators.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+ using namespace std;
+ int main(){
+
+    // arthematic operator
+
+    //  int a, b;
+    //  a=10;
+    //  b=4;
+    //  cout<<"sum of two no.= "<<(a+ b)<<endl;
+    //  cout<<" difference of two no.= "<<(a - b)<<endl;
+    //  cout<<" product of two no.= "<<(a* b)<<endl;
+    //  cout<<" division of two no.= "<<(a/ (double)b)<<endl;  //double krne pr point me aayega output
+
+     // relation operator
+
+    //  cout<< (5>1) <<endl; //true (1)
+    //  cout<< (5<1) <<endl; //false(0)
+    //  cout<< (5==1) <<endl; //false (0)
+    //  cout<< (5>=1) <<endl; //true (1)
+    //  cout<< (5<=1) <<endl; //false (0)
+    //  cout<< !(5==1) <<endl; //true (1)
+
+    //logical operator
+
+    cout<<((3<1)||(3>1))<<endl;  //true(1)
+    cout<<((3<1)||(3<1))<<endl;  //false(0)
+    cout<<((3<1)&&(3>1))<<endl;  //false(0) ek bhi false so false output
+    
+    return 0;
+ }

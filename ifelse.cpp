19841e00b2bd1290914bf3f0d55028f245c3417d
bhumisakshi7-cell <bhumisakshi7@gmail.com>
2026-08-03 22:54:23 +0530

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int n = -30;
+    // if(n>=0){
+    //     cout<<" n is positive\n";
+    //  }
+    //   else{
+    //        cout<<"n is negative\n";
+    //     }
+    // int age;
+    // cout<<"Enter your age";
+    // cin>>age;
+    // if(age>=18){
+    //     cout<<"you can eligible\n";
+    // }
+    // else{
+    //     cout<<"you can't vote\n";
+    // }
+    int n;
+    cout<<"Enter number : ";
+    cin>>n;
+    if(n%2==0){
+        cout<<"even\n";
+
+    }
+    else{
+        cout<<"odd\n";
+    }
+    }

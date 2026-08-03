3694fd20a8efa273bb4b2fb5232a927a64dabe2a
bhumisakshi7-cell <bhumisakshi7@gmail.com>
2026-08-03 23:03:30 +0530

    Add files via upload

---
+++ nestedloop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // for(int i =1 ; i<=5; i++){
+    //     cout<<"******"<<endl;
+    // }
+
+    int x=20;    // in one line star print
+    for(int i =1;i<=x;i++){
+        cout<<"*";
+    }
+    return 0;
+}

7ec8e75082f3b8567481c09bf5db39c2cb9d6c33
bhumisakshi7-cell <bhumisakshi7@gmail.com>
2026-08-03 22:35:15 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+ int num = 1; 
+    for(int i=0; i<n; i++){
+
+     for(int j =0; j<i; j++){
+          start;condition;update/increment
+         cout<< " ";
+     }
+     for(int j=0; j<n-i; j++){
+            cout<< (i+1);
+        }
+        cout<<endl;
+     
+    }

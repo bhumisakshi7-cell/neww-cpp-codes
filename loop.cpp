cdacfb1a385b5a1f54bab48a06bfe4fb23e52bc6
bhumisakshi7-cell <bhumisakshi7@gmail.com>
2026-08-03 23:04:10 +0530

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,67 @@
+ #include<iostream>
+ using namespace std;
+ int main(){
+    // int i =1;
+    // while(i<=5){
+    //     cout<<i<<" ";
+    //     i++;
+    // }
+    // cout<<endl;
+
+
+
+    // int n=8;
+    // for(int i=1; i<=n;i++){
+    //     cout<<i<<"  ";
+    // }
+
+
+    // int n=50;
+    // int sum =0;
+    // for(int i=1;i<=n;i++){
+    //     sum +=i;
+    // }
+    // cout<<"sum="<<sum<<endl;
+
+
+    // int n = 1;
+    // int sum = 0;
+    // while (n<=50){
+    // sum=sum+n;
+    // n++;
+    // cout<<sum<<endl;
+    // }
+
+
+//   int n = 10;
+
+//   //print odd no.
+
+//   for( int i=1 ; i<=n; i++){
+//    if(i%2!=0){
+//     cout<< i <<" "<<endl;
+//    }
+//   }
+
+
+// int n =50;
+// int oddsum=0;
+// for (int i =1;i<=n;i++){
+//   if(i%2!=0){
+//     oddsum+=i;
+//   }
+// }
+// cout<<"odd sum = "<<oddsum<<endl;
+    
+
+ int n = 10;
+  int i =1;
+ 
+do{
+    cout<< i<<" ";
+i++;
+}while(i<=n);
+cout<<endl;
+    return 0;
+    
+ }

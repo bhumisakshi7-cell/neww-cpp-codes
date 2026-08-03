24c1c0bedbc3f295be339c8c66655acc08cecbdc
bhumisakshi7-cell <bhumisakshi7@gmail.com>
2026-08-03 22:34:30 +0530

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age=20;
+    char grade='B';
+    float pi = 20.6f;
+    bool isSafe = true;
+    double price = 600.3;
+    cout<< age <<endl << grade <<endl << pi <<endl << isSafe <<endl << price <<endl;
+    return 0;
+}
